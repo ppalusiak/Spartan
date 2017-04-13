48e0ddab1e33fc0bc1a4f372c2035d5e1dc935ed
michaltrunek <michal.trunek@tipico.com>
2017-04-13 17:26:52 +0200

    codecept.conf.js extended file added

---
+++ runners/mobile/codecept.conf.js
@@ -0,0 +1,51 @@
+exports.config = {
+    tests: './tests/mobile/*welcomeBonus_test.js',
+    timeout: 30000,
+    output: './output',
+    helpers: {
+        WebDriverIO: {
+            url: 'http://localhost',
+            restart: true,
+            browser: 'chrome',
+            windowSize: '415x730',
+            waitForTimeout: 30000,
+            desiredCapabilities: {
+                browserName: 'chrome',
+                chromeOptions: {
+                    args: [
+                        'user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 8_0_2 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12A405 Safari/600.1.4'
+                    ]
+                },
+                mobileEmulationEnabled: true
+            },
+            coloredLogs: true
+        },
+        MyHelper: {
+            require: './helpers/custom_methods/custom_helper.js'
+        }
+    },
+    include: {
+        I: './helpers/steps_file.js',
+        loginPage: './helpers/mobile/pages/Login.js',
+        homePage: './helpers/mobile/pages/Home.js',
+        registrationPage: './helpers/mobile/pages/Registration.js',
+        termsConditionsPage: './helpers/mobile/pages/TermsConditions.js',
+        myAccountPage: './helpers/mobile/pages/MyAccount.js',
+        informationPage: './helpers/mobile/pages/Information.js',
+        footerPage: './helpers/mobile/pages/Footer.js',
+        betsPage: './helpers/mobile/pages/Bets.js',
+        switchPage: './helpers/mobile/pages/SwitchDomain.js',
+        firstDepositPage: './helpers/mobile/pages/FirstDeposit.js',
+        promoPopupFragment: './helpers/mobile/fragments/promoPopup.js',
+        cancelRegistrationFragment: './helpers/mobile/fragments/cancelRegistrationPopup.js'
+    },
+    bootstrap: false,
+    mocha: {
+        reporterOptions: {
+            reportDir: './output',
+            enableCharts: true,
+            reportTitle: 'Spartan Reporter'
+        }
+    },
+    name: 'Spartan'
+};
--- tests/mobile/mobile_welcomeBonus_test.js
+++ tests/mobile/mobile_welcomeBonus_test.js
@@ -3,7 +3,7 @@ Feature('Promo popup (mobile rebrush)', {retries: 1});
 Before((I, homePage) => {
 });
 
-xScenario('Promo Layout on all domains', (I, promoPopupFragment, homePage) => {
+Scenario('Promo Layout on all domains', (I, promoPopupFragment, homePage) => {
     let domains = ["https://mobile-hotfix.tipdev.com", "https://mobile-hotfix-de.tipdev.com", "https://mobile-hotfix-de-sh.tipdev.com", "https://mobile-hotfix-at.tipdev.com"];
     domains.forEach(function (element) {
         I.amOnPage(element);
@@ -28,21 +28,21 @@ Scenario('Close popup and see the home page', (I, promoPopupFragment, homePage) 
     }
 });
 
-xScenario('Sign up button -> registration section', (I, promoPopupFragment, registrationPage, homePage) => {
+Scenario('Sign up button -> registration section', (I, promoPopupFragment, registrationPage, homePage) => {
     I.amOnPage('https://mobile-hotfix.tipdev.com');
     homePage.hideSplashScreen();
     promoPopupFragment.goToRegistration();
     registrationPage.checkPage();
 });
 
-xScenario('Terms and Conditions button -> T&C section', (I, homePage, promoPopupFragment, termsConditionsPage) => {
+Scenario('Terms and Conditions button -> T&C section', (I, homePage, promoPopupFragment, termsConditionsPage) => {
     I.amOnPage('https://mobile-hotfix.tipdev.com');
     homePage.hideSplashScreen();
     promoPopupFragment.goToTermsConditions();
     termsConditionsPage.checkPage();
 });
 
-xScenario('Welcome promo is not displayed on navigation through the app / user logged out', (I, homePage, loginPage, footerPage, promoPopupFragment) => {
+Scenario('Welcome promo is not displayed on navigation through the app / user logged out', (I, homePage, loginPage, footerPage, promoPopupFragment) => {
     I.amOnPage('https://mobile-hotfix.tipdev.com');
     homePage.hideSplashScreen();
     promoPopupFragment.checkLayout();
@@ -54,7 +54,7 @@ xScenario('Welcome promo is not displayed on navigation through the app / user l
     promoPopupFragment.checkPromoVisible(false);
 });
 
-xScenario('Welcome promo is not displayed on domain redirection', (I, homePage, loginPage, switchPage, promoPopupFragment) => {
+Scenario('Welcome promo is not displayed on domain redirection', (I, homePage, loginPage, switchPage, promoPopupFragment) => {
     I.amOnPage('https://mobile-hotfix.tipdev.com');
     homePage.hideSplashScreen();
     promoPopupFragment.checkLayout();
